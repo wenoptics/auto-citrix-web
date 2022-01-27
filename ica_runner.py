c07410f6cc4ca74e3ec84eca19a94cb0a93364d6
wenop <wenoptics@gmail.com>
2022-01-27 16:11:42 -0500

    feat: Integrate with rich

--- ica_runner.py
+++ ica_runner.py
@@ -3,18 +3,27 @@ import shutil
 import subprocess
 import tempfile
 from pathlib import Path
+from typing import Union
 
-from rich import print
 
-
+logger = logging.getLogger(__name__)
 BIN_RUNNER = r"C:\Program Files (x86)\Citrix\ICA Client\wfcrun32.exe"
 
-print('Using runner from:', BIN_RUNNER)
 
+def run_ica(ica_file: Union[Path, str], runner=BIN_RUNNER):
+    logger.debug('Using runner from: {}'.format(BIN_RUNNER))
 
-def run_ica(ica_file: str):
     ica_file = Path(ica_file)
     with tempfile.TemporaryDirectory() as tempdir:
         test_file = Path(tempdir) / 'test.ica'
         shutil.copy(ica_file, test_file)
-        ret = subprocess.call([BIN_RUNNER, str(ica_file)])
+        ret = subprocess.call([str(runner), str(ica_file)])
+
+
+if __name__ == '__main__':
+    from rich.logging import RichHandler
+    logging.basicConfig(
+        level=logging.NOTSET,
+        format='[%(name)s] %(message)s',
+        handlers=[RichHandler(rich_tracebacks=True)]
+    )
--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import asyncio
 import contextlib
 import shutil
 from pathlib import Path
+import logging
 
-from rich import print
+import rich.spinner
 from playwright.async_api import Playwright, async_playwright, Page
-import config
+from rich.logging import RichHandler
 
-assert config.settings.get('auth', 'USERNAME')
-assert config.settings.get('auth', 'PASSWORD')
+import config
+from ica_runner import run_ica
+
+console = rich.console.Console()
+logger = logging.getLogger(__name__)
 
 
 @contextlib.asynccontextmanager
@@ -26,25 +30,31 @@ async def citrix_login(playwright: Playwright, username: str, password: str):
     await page.fill('input[id="passwd"]', password)
     await page.check('input[id="eula_check"]')
 
-    # Create a notice
-    await page.evaluate("""
-      const el = document.createElement("div");
-      el.appendChild(document.createTextNode("Waiting for login response..."));
-      el.style.padding = '2em';
-      el.style.fontSize = '2em';
-      el.style.margin = '1em';
-      el.style.border = '1px white solid';
-      el.style.color = 'white';
-      el.style.backgroundColor = 'rgba(100,100,100,0.8)';
-      el.style.textAlign = 'center';
+    # Login and wait for login success
+    with console.status("[bold blue]Waiting for login success...", spinner='dots') as status:
+        # Create a notice
+        await page.evaluate("""
+          const el = document.createElement("div");
+          el.appendChild(document.createTextNode("Waiting for login response..."));
+          el.style.padding = '2em';
+          el.style.fontSize = '2em';
+          el.style.margin = '1em';
+          el.style.border = '1px white solid';
+          el.style.color = 'white';
+          el.style.backgroundColor = 'rgba(100,100,100,0.8)';
+          el.style.textAlign = 'center';
+    
+          document.querySelector('#logonbelt-bottomshadow')
+            .appendChild(el);
+        """)
 
-      document.querySelector('#logonbelt-bottomshadow')
-        .appendChild(el);
-    """)
-    await page.click('input[id="Log_On"]')
+        console.log('You may need to respond to the login SSO request...')
+        await page.click('input[id="Log_On"]')
+        console.log('Waiting for login response...')
 
-    # Click text=Skip Check
-    await page.click("text=Skip Check")
+        # Click text=Skip Check
+        await page.click("text=Skip Check")
+        console.log('Skip Check applied.')
     # assert page.url == "https://ctxstore.ansys.com/Citrix/storeWeb-ext/"
 
     yield page
@@ -74,19 +84,25 @@ async def start_citrix_app(playwright: Playwright, app_start='Remote Desktop Man
             # app_image = await ele.locator('img.storeapp-icon').get_attribute('src')
             app_map[app_name] = el_app_items.nth(idx)
 
-        print('Available apps', list(app_map.keys()))
+        logger.info(f'Available apps ({len(app_map)}): {list(app_map.keys())}')
         if app_start not in app_map:
             raise ValueError(f'Not found app named "{app_start}"')
 
-        async with page.expect_download() as download_info:
-            await app_map[app_start].locator('.storeapp-details-link').click()
-        download = await download_info.value
-        # waits for download to complete
-        path = await download.path()
-        print(f'Downloaded to "{path}" (from "{download.url}")')
+        with console.status(f'[bold blue]Downloading app "{app_start}"...', spinner='dots') as status:
+            async with page.expect_download() as download_info:
+                console.log('Locating app...')
+                await app_map[app_start].locator('.storeapp-details-link').click()
+            download = await download_info.value
+            # waits for download to complete
+            console.log(f'Downloading app (from "{download.url}")...')
+            path = await download.path()
+            console.log('Done')
 
+        logger.info(f'App downloaded to "{path}"')
         dst = Path(r'C:\Users\wenop\OneDrive\Desktop')
-        shutil.copy(path, dst / 'start.ica')
+        _ica = dst / 'start.ica'
+        shutil.copy(path, _ica)
+        run_ica(_ica)
 
 
 async def main() -> None:
@@ -94,4 +110,11 @@ async def main() -> None:
         await start_citrix_app(playwright)
 
 
-asyncio.run(main())
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.NOTSET,
+        format='[%(name)s] %(message)s',
+        handlers=[RichHandler(rich_tracebacks=True)]
+    )
+
+    asyncio.run(main())
